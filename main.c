82227569d840ec9fafd57a98fc72a6f09a2f7af3
skrshe <sanshonekrashe@gmail.com>
2021-07-04 23:28:32 +1000

    code cleaning + started on jumping logic

--- main.c
+++ main.c
@@ -12,13 +12,23 @@ struct {
     SDL_GameController *gpad;
 } game;
 
+//struct Thing {
+//    SDL_Surface *img;
+//    SDL_Texture *tex;
+//    SDL_FreeSurface(img);
+
+//    SDL_Rect src = {0,0,0,0};
+//    SDL_Rect dst = {0,0,0,0};
+//    int scroll = 0;
+//};
+
 // void sdlInit() { }
 // void gameInit() { }
 // void gameUpdate() { }
 
-int main(int argc,char **argv) {
+int main(int argc, char **argv) {
     // sdlInit();
-    SDL_Init(SDL_INIT_VIDEO);
+    // SDL_Init(SDL_INIT_VIDEO);
 
     game.win = SDL_CreateWindow(
             "Geary guy",
@@ -57,94 +67,106 @@ int main(int argc,char **argv) {
     SDL_Texture *p_tex = SDL_CreateTextureFromSurface(game.ren, player);
     SDL_FreeSurface(player);
 
-    int count = 0;
 
-    SDL_Rect distant_dest ={ 0, 0, WIDTH, HEIGHT };
+    SDL_Rect distant_dest = { 0, 0, WIDTH, HEIGHT };
 
     int scrollfar = 0;
-    SDL_Rect far_dest; far_dest.w = WIDTH; far_dest.h = 60; far_dest.y = 5;
-    SDL_Rect far_dest2; far_dest2.w = WIDTH; far_dest2.h = 60; far_dest2.y = 5;
+    SDL_Rect far_dest  = { .w = WIDTH, .h = 60, .y = 5 };
+    SDL_Rect far_dest2 = { .w = WIDTH, .h = 60, .y = 5 };
 
     int scrollnear = 0;
-    SDL_Rect near_dest; near_dest.w = WIDTH; near_dest.h = 36; near_dest.y = 40;
-    SDL_Rect near_dest2; near_dest2.w = WIDTH; near_dest2.h = 36; near_dest2.y = 40;
+    SDL_Rect near_dest  = { .w = WIDTH, .h = 36, .y = 40 };
+    SDL_Rect near_dest2 = { .w = WIDTH, .h = 36, .y = 40 };
 
     int scrollground = 0;
-    SDL_Rect ground_dest; ground_dest.w = WIDTH; ground_dest.h = 95; ground_dest.y = 49;
-    SDL_Rect ground_dest2; ground_dest2.w = WIDTH; ground_dest2.h = 95; ground_dest2.y = 49;
+    SDL_Rect ground_dest  = { .w = WIDTH, .h = 95, .y = 49 };
+    SDL_Rect ground_dest2 = { .w = WIDTH, .h = 95, .y = 49 };
 
     int scrollroad = 0;
-    SDL_Rect road_dest; road_dest.w = 251; road_dest.h = 28; road_dest.y = 76;
-    SDL_Rect road_dest2; road_dest2.w = 251; road_dest2.h = 28; road_dest2.y = 76;
-    SDL_Rect road_dest3; road_dest3.w = 251; road_dest3.h = 28; road_dest3.y = 76;
+    SDL_Rect road_dest  = { .w = 251, .h = 28, .y = 76 };
+    SDL_Rect road_dest2 = { .w = 251, .h = 28, .y = 76 };
+    SDL_Rect road_dest3 = { .w = 251, .h = 28, .y = 76 };
 
-    SDL_Rect p_src; p_src.y = 0; p_src.w = 32; p_src.h = 32;
-    SDL_Rect p_dest; p_dest.x = 20; p_dest.y = 60; p_dest.w = 32; p_dest.h = 32;
+    SDL_Rect p_src  = {.x = 0, .y = 0, .w = 32, .h = 32 };
+    SDL_Rect p_dest = {.x = 20, .y = 60, .w = 32, .h = 32 };
 
     int quit = 0;
     while (!quit) {
         //gameUpdate();
         // Tic
+        const Uint32 start = SDL_GetTicks();
+        int jump = 0;
+
         SDL_Event e;
         while (SDL_PollEvent(&e) > 0) {
-            if (e.type == SDL_QUIT) { quit = 1; }
-            if (e.key.keysym.scancode ==
-                    SDL_SCANCODE_ESCAPE) {
-                quit = 1;
+            switch (e.type) {
+                case SDL_QUIT: quit = 1; break;
+
+                case SDL_KEYDOWN: {
+                    switch (e.key.keysym.sym) {
+                        case SDLK_ESCAPE: quit = 1; break;
+                        case SDLK_SPACE: jump = 1; break;
+                    }
+                }
             }
         }
 
-        int frame = (SDL_GetTicks() / 150) % 6;
-        p_src.x = frame * p_src.w;
+        // TODO(anu): fix jump
+        // TODO(anu): fix animation
 
-        far_dest.x = scrollfar;
-        far_dest2.x = scrollfar + WIDTH;
+        int jumpheight = 0;
 
-        near_dest.x = scrollnear;
-        near_dest2.x = scrollnear + WIDTH;
+        if (jump) {
+            jumpheight += 7;
+            jump = 0;
+        }
 
-        ground_dest.x = scrollground;
-        ground_dest2.x = scrollground + WIDTH;
+        if (jumpheight > 0) jumpheight--;
+        p_dest.y = 60;// - jumpheight;
 
-        road_dest.x = scrollroad;
-        road_dest2.x = scrollroad + road_dest.w;
+        if (p_src.x == 6 * p_src.w) p_src.x = 0;
+
+        far_dest.x = scrollfar; far_dest2.x = scrollfar + WIDTH;
+        near_dest.x = scrollnear; near_dest2.x = scrollnear + WIDTH;
+        ground_dest.x = scrollground; ground_dest2.x = scrollground + WIDTH;
+        road_dest.x = scrollroad; road_dest2.x = scrollroad + road_dest.w;
         road_dest3.x = scrollroad + (road_dest.w * 2);
 
-        count++;
-        if (count % 4 == 0) { scrollroad--; }
-        if (count % 7 == 0) { scrollground--; }
-        if (count % 20 == 0) { scrollnear--;}
-        if (count % 40 == 0) { scrollfar--;  }
+        int count; count++;
+        if (count % 1 == 0)  scrollroad--;
+        if (count % 1 == 0)  {
+            scrollground--;
+        }
+        if (count % 5 == 0) {
+            scrollnear--;
+            p_src.x += p_src.w;
+        }
+        if (count % 17 == 0) scrollfar--;
         // else if (count == 140) {  count = 0;}
 
-        if (scrollfar < -WIDTH) { scrollfar = 0; }
-        if (scrollnear < -WIDTH) { scrollnear = 0; }
-        if (scrollground < -WIDTH) { scrollground = 0; }
-        if (scrollroad < -road_dest.w) { scrollroad = 0; }
+        if (scrollfar    < -WIDTH)       scrollfar = 0;
+        if (scrollnear   < -WIDTH)       scrollnear = 0;
+        if (scrollground < -WIDTH)       scrollground = 0;
+        if (scrollroad   < -road_dest.w) scrollroad = 0;
 
         // Draw
         SDL_RenderClear(game.ren);
         SDL_SetRenderDrawColor(game.ren, 0, 0, 0, 255);
 
         SDL_RenderCopy(game.ren, distant_tex, NULL, &distant_dest);
-
-        SDL_RenderCopy(game.ren, far_tex, NULL, &far_dest);
-        SDL_RenderCopy(game.ren , far_tex, NULL, &far_dest2);
-
-        SDL_RenderCopy(game.ren, near_tex, NULL, &near_dest);
-        SDL_RenderCopy(game.ren , near_tex, NULL, &near_dest2);
-
-        SDL_RenderCopy(game.ren, ground_tex, NULL, &ground_dest);
-        SDL_RenderCopy(game.ren , ground_tex, NULL, &ground_dest2);
-
-        SDL_RenderCopy(game.ren, road_tex, NULL, &road_dest);
-        SDL_RenderCopy(game.ren, road_tex, NULL, &road_dest2);
-        SDL_RenderCopy(game.ren, road_tex, NULL, &road_dest3);
-
+        SDL_RenderCopy(game.ren, far_tex, NULL, &far_dest); SDL_RenderCopy(game.ren , far_tex, NULL, &far_dest2);
+        SDL_RenderCopy(game.ren, near_tex, NULL, &near_dest); SDL_RenderCopy(game.ren , near_tex, NULL, &near_dest2);
+        SDL_RenderCopy(game.ren, ground_tex, NULL, &ground_dest); SDL_RenderCopy(game.ren , ground_tex, NULL, &ground_dest2);
+        SDL_RenderCopy(game.ren, road_tex, NULL, &road_dest); SDL_RenderCopy(game.ren, road_tex, NULL, &road_dest2); SDL_RenderCopy(game.ren, road_tex, NULL, &road_dest3);
         SDL_RenderCopy(game.ren, p_tex, &p_src, &p_dest);
 
         SDL_RenderPresent(game.ren);
-    }
 
+        const Uint32 duration = SDL_GetTicks() - start;
+        const Uint32 delta_time_ms = 1000 / 60;
+        if (duration < delta_time_ms)
+            SDL_Delay(delta_time_ms - duration);
+    }
+    SDL_Quit();
     return 0;
 }
